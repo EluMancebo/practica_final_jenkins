80ae7e425354ba1b572281fc123c20a0e87cddc9
EluM <emango0298@gmail.com>
2025-01-17 23:55:29 +0100

    some fixes

--- jenkins-pipeline-project/jenkinsScripts/push_changes.sh
+++ jenkins-pipeline-project/jenkinsScripts/push_changes.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-
-
-git config --global user.name "EluM"
+git config --global user.name "EluMancebo"
 git config --global user.email "emango0298@gmail.com"
 
-
 cd jenkins-pipeline-project
-
-
-if git show-ref --verify --quiet refs/heads/ci_jenkins; then
-    git checkout ci_jenkins
-else
-    git checkout -b ci_jenkins origin/ci_jenkins
-fi
-
-
-git add README.md
+git add jenkins-pipeline-project/README.md
 
 
 git commit -m "Pipeline ejecutada por $EXECUTOR, el motivo es: $MOTIVO"
@@ -25,9 +13,9 @@ git push origin ci_jenkins
 
 
 if [ $? -eq 0 ]; then
-    echo "Push: success"
-    exit 0
+  echo "Push: success"
+  exit 0
 else
-    echo "Push: failed"
-    exit 1
+  echo "Push: failed"
+  exit 1
 fi
